b6bcf231161e73c9263e9991b7e352c9e63cb364
Kasna <40149557+Kasna@users.noreply.github.com>
2019-09-14 14:17:55 +0630

    Viber bot

---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const ViberBot = require('viber-bot').Bot;
+const BotEvents = require('viber-bot').Events;
+const express = require('express');
+const app = express();
+const requestify = require('requestify');
+const TextMessage = require('viber-bot').Message.Text;
+const UrlMessage = require('viber-bot').Message.Url;
+const ContactMessage = require('viber-bot').Message.Contact;
+const PictureMessage = require('viber-bot').Message.Picture;
+const VideoMessage = require('viber-bot').Message.Video;
+const LocationMessage = require('viber-bot').Message.Location;
+const StickerMessage = require('viber-bot').Message.Sticker;
+const RichMediaMessage = require('viber-bot').Message.RichMedia;
+const KeyboardMessage = require('viber-bot').Message.Keyboard;
+const PAT = 'EAAGZBZCPOeEz8BADP2o6eKUrLyoWpSMkalbi92m9tylUymQtrWqwOZAa9WqUQaZAayEUlCvIU1wMxEUoA6rwi3kp3QlBmjBaOE7Bt4l3JZCITaWB5ZCvnm4TBZBMeeYBRm4fx5zOLHjjhnEunRupZB4zKqy16dz9H5Q8HsNopPRCJQZDZD';
+
+const bot = new ViberBot({
+	authToken: '4a4bad117867d111-549a62a556c2e0a7-349c9be268cfcf98',
+	name: "Tricycle Service",
+	avatar: "https://www.google.com/imgres?imgurl=https://image.shutterstock.com/image-vector/pedicab-vector-icon-logo-260nw-502101706.jpg&imgrefurl=https://www.shutterstock.com/search/tricycle%2Blogo&tbnid=Ba49em_IAJdWiM&vet=1&docid=XwpyuXO0g4vcjM&w=368&h=280&q=tricycle+logo&hl=en-mm&source=sh/x/im" // It is recommended to be 720x720, and no more than 100kb.
+});
+
+const KEYBOARD_FRAME = {
+	"Type": "keyboard",
+	"DefaultHeight": false,
+	"BgColor": "#ffffff",
+	"Buttons": [
+		
+	]
+};
+const minApiVersion = 7;
+
+const userprofile = []
+// Perfect! Now here's the key part:
+bot.on(BotEvents.CONVERSATION_STARTED, (userProfile, isSubscribed, context, onFinish) => {
+	const uPF = userProfile.userProfile
+  console.log(uPF);
+	userprofile.push(uPF);
+	bot.sendMessage(uPF,new TextMessage('Hello '+uPF.name+'! Welcome to Tricycle Service!',{
+	"Type": "keyboard",
+	"DefaultHeight": false,
+	"BgColor": "#ffffff",
+	"Buttons": [
+		{
+			"Columns": 3,
+			"Rows": 1,
+			"BgColor": "#000000",
+			"Text": "<font color='#ffffff'>Customer</font>",
+			"InputFieldState": "hidden",
+			"TextHAlign": "center",
+			"TextVAlign": "middle",
+			"ActionType": "reply",
+			"TextSize": "large",
+			"ActionBody": "Customer"
+		},
+		{
+			"Columns": 3,
+			"Rows": 1,
+			"BgColor": "#000000",
+			"Text": "<font color='#ffffff'>Tricycle Register</font>",
+			"InputFieldState": "hidden",
+			"TextHAlign": "center",
+			"TextVAlign": "middle",
+			"ActionType": "reply",
+			"TextSize": "large",
+			"ActionBody": "Driver"
+		}
+	]
+}),["UserType"]).catch(function(error){
+		console.log('error', error);
+	});
+});
+
+
+bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
+	// Echo's back the message to the client. Your bot logic should sit here.
+	console.log(message)
+	if(message.text){
+		var userInput = message.text
+		var trackingData = message.trackingData
+	}
+	console.log("userinput", userInput)
+	console.log("trackingData", trackingData)
