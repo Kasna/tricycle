a33b8c1778fd36736e856a3c74eda1ea822f34d2
Kasna <40149557+Kasna@users.noreply.github.com>
2019-11-20 16:03:12 +0630

    Update index.js

--- index.js
+++ index.js
@@ -349,7 +349,7 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
 			"Text": "<font color='#000000'>Home</font>"
 		}
 	]
-			},"","","",minApiVersion))],[`{lat:${slat}, lon:${slon}, elat:${lat}, elon: ${lon} }` ] )
+			},"","","",minApiVersion))],[`lat:${slat}, lon:${slon}, elat:${lat}, elon: ${lon} ` ] )
 	}
 	if (userInput == '') {   
 		bot.sendMessage(uPF,[
